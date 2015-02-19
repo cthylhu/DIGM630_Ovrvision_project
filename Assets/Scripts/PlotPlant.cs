5ce2f4f9f86140e11434be4fd78a1e8aef616b12
cthylhu <cyl33@n1-142-55.dhcp.drexel.edu>
2015-02-19 18:33:05 -0500

    Begin scripting PlotPlant
    - New script PlotPlant for detecting in AR when player plants a seed on
    a planet (button press for now)
    - Added TestPlantPlot objects

---
+++ Assets/Scripts/PlotPlant.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class PlotPlant : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("a")) {
+			//Debug.Log ("Button pressed!");
+			Renderer[] children = GetComponentsInChildren<Renderer>();
+			foreach(Renderer r in children) {
+				//Debug.Log("Child: " + r.name);
+				if (r.name == "Test_Plant_Planet"){
+					r.renderer.enabled = true;
+				}
+				else {
+					r.renderer.enabled = false;
+				}
+			}
+
+		}
+	}
+}
