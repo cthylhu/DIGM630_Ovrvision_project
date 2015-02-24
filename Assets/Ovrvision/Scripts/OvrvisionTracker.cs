5501f617d492d9635cb0fcec6882b9021322c5fa
cthylhu <cyl33@n1-142-55.dhcp.drexel.edu>
2015-02-24 00:25:45 -0500

    Implement new Marker location tracking (not working)

--- Assets/Ovrvision/Scripts/OvrvisionTracker.cs
+++ Assets/Ovrvision/Scripts/OvrvisionTracker.cs
@@ -16,16 +16,34 @@ public class OvrvisionTracker : MonoBehaviour {
 	// Tracker initialization
 	public void Start()
 	{
-		if (GameObject.Find("LeftEyeAnchor"))
-			this.transform.parent = GameObject.Find("LeftEyeAnchor").transform;
+		/*if (GameObject.Find("LeftEyeAnchor"))
+			this.transform.parent = GameObject.Find("LeftEyeAnchor").transform;*/
 	}
 
 	// UpdateTracker
 	public void UpdateTransform (float[] markerGet, int elementNo) {
 		int i = elementNo * MARKERGET_ARG10;
-		this.transform.localPosition = new Vector3(markerGet[i + 1], markerGet[i + 2], markerGet[i + 3]);
-		//this.transform.localPosition = new Vector3(0, 0, 0);
-		this.transform.localRotation = new Quaternion (markerGet[i+4],markerGet[i+5],markerGet[i+6],markerGet[i+7]);
+
+		//Debug.Log ("Transform Vector: " + markerGet [i + 1]+", "+ markerGet [i + 2]+", "+ markerGet [i + 3]);
+		//Debug.Log ("Transform Quat: " + markerGet [i + 4]+", "+ markerGet [i + 5]+", "+ markerGet [i + 6]+", "+ markerGet [i + 7]);
+
+
+
+		//this.transform.localPosition = new Vector3(markerGet[i + 1], markerGet[i + 2], markerGet[i + 3]);
+		//this.transform.localRotation = new Quaternion (markerGet[i+4],markerGet[i+5],markerGet[i+6],markerGet[i+7]);
+
+		//Debug.Log ("Position of LeftEyeAnchor: " + GameObject.Find ("LeftEyeAnchor").transform.position);
+		this.transform.position = new Vector3(markerGet[i + 1], markerGet[i + 2], markerGet[i + 3]) + GameObject.Find("LeftEyeAnchor").transform.position;
+		//Debug.Log ("Position of Cube: " + this.transform.position);
+
+		this.transform.rotation =  new Quaternion (markerGet[i+4],markerGet[i+5],markerGet[i+6],markerGet[i+7]);
+	
+	}
+
+	public void Update(){
+		if (Input.GetKeyDown ("a")) {
+			Debug.Log ("Current Vector: "+this.transform.position);
+		}
 	}
 
 	public void UpdateTransformNone () {
